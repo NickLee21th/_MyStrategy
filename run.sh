54d51a4daa9b3656f2c94f208a755ec5b68b40d4
Ubuntu <ubuntu@ip-172-26-8-252.ap-southeast-1.compute.internal>
2021-03-28 06:17:23 +0000

    response -0328

---
+++ run.sh
@@ -0,0 +1,21 @@
+ls
+nohup python3 main.py >output_0327_Ma5Ma10_base bch3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base bch3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base bsv3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base bsv3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base btc3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base btc3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base eos3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base eos3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base eth3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base eth3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base fil3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base fil3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base link3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base link3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base ltc3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base ltc3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base xrp3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base xrp3susdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base zec3lusdt 2>&1 &
+nohup python3 main.py >output_0327_Ma5Ma10_base zec3susdt 2>&1 &
